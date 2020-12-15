52900c6ed5c03456ff3b46121b20cadbbc895ca7
abelwrg <abelw@outlook.com>
2020-12-15 14:23:33 +0800

    adjust some view

--- WebApplication/Controllers/HomeController.cs
+++ WebApplication/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your about page.";
+            ViewBag.Message = "Popular Flight Sectors: Kolkata to Delhi Flight |Hyderabad to Delhi Flight |Chennai to Hyderabad Flight |Delhi to Guwahati Flight |Lucknow to Delhi Flight |Nagpur to Mumbai Flight |Ranchi to Delhi Flight |Ahmedabad to Goa Flight |Mumbai to Chandigarh Flight |Pune to Kolkata Flight |Bangalore to Bhubaneshwar Flight |Bangalore to Guwahati Flight |Chennai to Goa Flight |Chennai to Kolkata Flight |Delhi to Jaipur Flight |Delhi to Leh Flight |Hyderabad to Goa Flight |Bangalore to Ranchi Flight |Delhi to Bagdogra Flight |Srinagar to Delhi Flight ";
 
             return View();
         }
---
+++ WebApplication/Models/Flights.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication.Models
+{
+    public class Flight
+    {
+        public int ID { get; set; }
+        public string Departure { get; set; }
+
+        public string Distination { get; set; }
+
+        [Display(Name = "Departure Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime DepartureDate { get; set; }
+        public DateTime ReturnDate { get; set; }
+        public int NumOfPassenger { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class FlightDBContext : DbContext
+    {
+        public DbSet<Flight> Flights { get; set; }
+    }
+
+}
